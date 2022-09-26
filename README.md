77d298bd3899a2c8ade9083f18682f7216cabde4
TK-wsh <ghr3ndkdy@gmail.com>
2022-09-26 12:02:15 +0900

    sss作成

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # javascript
-html:5を入力、自動でひな形生成
-lang = ja
-bodyの中にscript
-対話型の入力　s1 = prompt("s1 = ","")*1
-function 関数名(引数)　{ return 定数 }
+javascriptにreact入れる　sss.html sss.js
---
+++ sss.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const domContainer = document.querySelector('#sss_container');
+const root = ReactDOM.createRoot(domContainer);
+//root.render(<h1>Hello, world!</h1>);
+root.render("aaaaaaaa");
